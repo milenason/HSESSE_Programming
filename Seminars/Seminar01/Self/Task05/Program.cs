79d95f7bb2dacb713ac1af271ea6781a8f72c782
Milena Son <son-milena@mail.ru>
2023-09-13 00:13:24 +0300

    Task05

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,14 @@
-﻿// Your code here!
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите R: ");
+        int R = int.Parse(Console.ReadLine());
+        Console.Write("Введите U: ");
+        int U = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Сила тока: {U / R}");
+        Console.WriteLine($"Мощность цепи: {Math.Pow(U, 2) / R}");
+    }
+}
