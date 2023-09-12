94c0069ced4a52bc245147d3f6e0eb0a768f6653
Milena Son <son-milena@mail.ru>
2023-09-13 00:19:57 +0300

    Task06

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите длину катета1: ");
+        int k1 = int.Parse(Console.ReadLine());
+        Console.Write("Введите длину катета2: ");
+        int k2 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Гипотенуза: {Math.Sqrt(k1*k1 + k2*k2)}");
+    }
+}
