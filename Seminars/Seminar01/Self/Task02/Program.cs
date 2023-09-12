14ee892024b8357666c9ae5f97d42eeb79ee0c00
Milena Son <son-milena@mail.ru>
2023-09-13 00:08:06 +0300

    task02

--- Seminars/Seminar01/Self/Task02/Program.cs
+++ Seminars/Seminar01/Self/Task02/Program.cs
@@ -1,1 +1,13 @@
-﻿// Your code here!
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string name, surname, patronymic;
+        name = Console.ReadLine();
+        surname = Console.ReadLine();
+        patronymic = Console.ReadLine();
+        Console.WriteLine(name + " " + surname + " " + patronymic);
+    }
+}
